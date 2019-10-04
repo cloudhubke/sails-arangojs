c970dc5a78d758a329d1a7068ca0ed3711eab672
Bernard Gaitho <gaithoben@gmail.com>
2019-10-04 17:55:04 +0300

    Transaction

--- helpers/create-each.js
+++ helpers/create-each.js
@@ -182,15 +182,11 @@ module.exports = require('machine').build({
           collection = graph.edgeCollection(`${statement.tableName}`);
         }
 
-        console.log('====================================');
-        console.log('st', statement.values);
-        console.log('====================================');
-
         const results = [...(statement.values || [])].map(
           value => new Promise(async (resolve) => {
-            const record = await collection.save(value);
-            resolve({ new: record });
-          }),
+              const record = await collection.save(value);
+              resolve({ new: record });
+            }),
         );
 
         result = await Promise.all(results);
