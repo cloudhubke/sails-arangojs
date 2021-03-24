004e5356819708acbbf04c20ddb73b5f42896cf3
Bernard Gaitho <gaithoben@gmail.com>
2021-03-24 20:31:44 +0300

    Add aliases to object methods

--- helpers/private/schema/StaticMethods.js
+++ helpers/private/schema/StaticMethods.js
@@ -43,6 +43,123 @@ if (!global.getDocument) {
 }
 
 module.exports = (globalId, keyProps, cache, gIds) => {
+  const create = async function (params, dsName) {
+    try {
+      if (params.Email) {
+        params.Email = `${params.Email}`.toLocaleLowerCase().trim();
+      }
+
+      const doc = await global[`${globalId}Object`][`find${globalId}`](
+        {
+          id: params.id,
+        },
+        dsName
+      );
+
+      if (doc) {
+        throw new Error(`Document with same id already exists`);
+      }
+
+      let newdoc;
+
+      if (dsName) {
+        newdoc = await global[`_${globalId}`](dsName).create(params).fetch();
+      } else {
+        newdoc = await global[globalId].create(params).fetch();
+      }
+
+      if (newdoc) {
+        const doc = global[`${globalId}Object`].initialize(newdoc, dsName);
+        return doc;
+      }
+      return null;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const findOne = async function (params, dsName) {
+    try {
+      const { id, ...otherprops } = params || {};
+
+      let doc;
+      if (id) {
+        if (dsName) {
+          doc = await global[`_${globalId}`](dsName).findOne({ id: id });
+        } else {
+          doc = await global[`${globalId}`].findOne({ id: id });
+        }
+      }
+
+      for (let prop in otherprops) {
+        if (otherprops[prop] && !doc) {
+          if (dsName) {
+            doc = await global[`_${globalId}`](dsName).findOne({
+              [prop]: otherprops[prop],
+            });
+          } else {
+            doc = await global[`${globalId}`].findOne({
+              [prop]: otherprops[prop],
+            });
+          }
+        }
+      }
+
+      if (doc) {
+        if (dsName) {
+          return global[`${globalId}Object`].initialize(doc, dsName);
+        }
+        return global[`${globalId}Object`].initialize(doc);
+      }
+
+      return null;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const getOne = async function (params, dsName) {
+    try {
+      const { id } = params || {};
+
+      if (cache) {
+        if (dsName) {
+          if (
+            id &&
+            global[`${globalId}Object`][`Available${globalId}s`][
+              `${dsName}/${id}`
+            ]
+          ) {
+            return global[`${globalId}Object`][`Available${globalId}s`][
+              `${dsName}/${id}`
+            ];
+          }
+        } else {
+          if (id && global[`${globalId}Object`][`Available${globalId}s`][id]) {
+            return global[`${globalId}Object`][`Available${globalId}s`][id];
+          }
+        }
+      }
+
+      const obj = await global[`${globalId}Object`][`find${globalId}`](
+        params,
+        dsName
+      );
+
+      if (obj && obj.id) {
+        obj.saveToCache();
+      } else {
+        throw new Error(
+          `${globalId} not available in ${dsName || 'default'} datastore`
+        );
+      }
+
+      return obj;
+    } catch (error) {
+      throw error;
+    }
+  };
+
   return {
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     // STATIC METHODS
@@ -73,125 +190,10 @@ module.exports = (globalId, keyProps, cache, gIds) => {
         throw error;
       }
     },
-    [`get${globalId}`]: async function (params, dsName) {
-      try {
-        const { id } = params || {};
 
-        if (cache) {
-          if (dsName) {
-            if (
-              id &&
-              global[`${globalId}Object`][`Available${globalId}s`][
-                `${dsName}/${id}`
-              ]
-            ) {
-              return global[`${globalId}Object`][`Available${globalId}s`][
-                `${dsName}/${id}`
-              ];
-            }
-          } else {
-            if (
-              id &&
-              global[`${globalId}Object`][`Available${globalId}s`][id]
-            ) {
-              return global[`${globalId}Object`][`Available${globalId}s`][id];
-            }
-          }
-        }
-
-        const obj = await global[`${globalId}Object`][`find${globalId}`](
-          params,
-          dsName
-        );
-
-        if (obj && obj.id) {
-          obj.saveToCache();
-        } else {
-          throw new Error(
-            `${globalId} not available in ${dsName || 'default'} datastore`
-          );
-        }
-
-        return obj;
-      } catch (error) {
-        throw error;
-      }
-    },
-
-    [`create${globalId}`]: async function (params, dsName) {
-      try {
-        if (params.Email) {
-          params.Email = `${params.Email}`.toLocaleLowerCase().trim();
-        }
-
-        const doc = await global[`${globalId}Object`][`find${globalId}`](
-          {
-            id: params.id,
-          },
-          dsName
-        );
-
-        if (doc) {
-          throw new Error(`Document with same id already exists`);
-        }
-
-        let newdoc;
-
-        if (dsName) {
-          newdoc = await global[`_${globalId}`](dsName).create(params).fetch();
-        } else {
-          newdoc = await global[globalId].create(params).fetch();
-        }
-
-        if (newdoc) {
-          const doc = global[`${globalId}Object`].initialize(newdoc, dsName);
-          return doc;
-        }
-        return null;
-      } catch (error) {
-        throw error;
-      }
-    },
-
-    [`find${globalId}`]: async function (params, dsName) {
-      try {
-        const { id, ...otherprops } = params || {};
-
-        let doc;
-        if (id) {
-          if (dsName) {
-            doc = await global[`_${globalId}`](dsName).findOne({ id: id });
-          } else {
-            doc = await global[`${globalId}`].findOne({ id: id });
-          }
-        }
-
-        for (let prop in otherprops) {
-          if (otherprops[prop] && !doc) {
-            if (dsName) {
-              doc = await global[`_${globalId}`](dsName).findOne({
-                [prop]: otherprops[prop],
-              });
-            } else {
-              doc = await global[`${globalId}`].findOne({
-                [prop]: otherprops[prop],
-              });
-            }
-          }
-        }
-
-        if (doc) {
-          if (dsName) {
-            return global[`${globalId}Object`].initialize(doc, dsName);
-          }
-          return global[`${globalId}Object`].initialize(doc);
-        }
-
-        return null;
-      } catch (error) {
-        throw error;
-      }
-    },
+    [`get${globalId}`]: getOne,
+    [`create${globalId}`]: create,
+    [`find${globalId}`]: findOne,
 
     initialize: function initialize(doc, dsName) {
       try {
@@ -270,6 +272,14 @@ module.exports = (globalId, keyProps, cache, gIds) => {
         throw error;
       }
     },
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // ALIASES
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    findOne,
+    getOne,
+    create,
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     // PROTOTYPES
